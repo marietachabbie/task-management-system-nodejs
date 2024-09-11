2e9e37d56c9e3674fa559121b0368fc26df8ba63
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2024-09-11 22:28:48 +0400

    Single quote fix

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const taskController = require('../controllers/tasks');
+const express = require("express");
+const taskController = require("../controllers/tasks");
 
 const router = express.Router();
 
-router.get('/', taskController.getTasks);
-router.post('/', taskController.createTask);
-router.get('/:id', taskController.getTask);
-router.post('/:id', taskController.updateStatus);
+router.get("/", taskController.getTasks);
+router.post("/", taskController.createTask);
+router.get("/:id", taskController.getTask);
+router.post("/:id", taskController.updateStatus);
 
 module.exports = router;
